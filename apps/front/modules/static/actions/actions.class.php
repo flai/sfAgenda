685e95690ba69796ed721bdf6d7bf5e3db50670e
Cristina Quintana Cano <quintana.cano@gmail.com>
2011-08-03 18:58:53 +0200

    Creación de la estructura proyecto (configuración, creación de aplicaciones y módulos).

---
+++ apps/front/modules/static/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * static actions.
+ *
+ * @package    sfAgenda
+ * @subpackage static
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class staticActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/private/var/www/symfony-1.4.8/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
